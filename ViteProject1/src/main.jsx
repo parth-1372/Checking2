1c1312dcc80e789d25fa450699fed0e33339d148
Parth <parthmungra1372@gmail.com>
2024-01-26 14:05:16 +0530

    some change

--- ViteProject1/src/main.jsx
+++ ViteProject1/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
  
-
+// function NewApp (){
+//   return (
+//     <h1>Changed through main.js </h1>
+//   )
+// }
+export default NewApp
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <NewApp/> */}
   </React.StrictMode>,
 )
